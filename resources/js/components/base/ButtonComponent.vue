6f8efe2b9ef2caaba967caf7ecec459de714e78a
Hanif <nurhanif2312@gmail.com>
2024-11-25 21:59:47 +0700

    Initial commit for OpenYourMind-2

---
+++ resources/js/components/base/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineEmits(['actions'])
+defineProps({
+    title: {
+        type: String,
+        required: false,
+        default: 'Contact Us'
+    }, class: {
+        type: String,
+        // ? is relate with responsive button
+        default: 'max-[374px]:hidden px-2 md:px-6 py-2 text-sm font-medium font-secondary text-center text-white bg-secondary-green rounded-lg md:rounded-full hover:shadow-lg transition-all'
+    }, disabled: {
+        required: false
+    }
+})
+</script>
+<template>
+    <button type="button" :disabled="disabled" @click="$emit('actions')" :class="class">
+        {{ title }}
+        <slot />
+    </button>
+</template>
